32986537a67af3ca85d89fc0d6511b586cfde46b
Sam David Lazarus <samdavidlazarus@gmail.com>
2020-01-07 16:55:07 +0530

    Update UploadFileUsingRobot.java

--- challenges/UploadFileUsingRobot.java
+++ challenges/UploadFileUsingRobot.java
@@ -11,15 +11,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UploadFileUsingRobot {
 	public static void main(String[] args) throws InterruptedException, AWTException {
+		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.naukri.com/");	
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);		
-		driver.findElementById("input_resumeParser").click();
+
+		WebElement uploadButton = driver.findElementByXPath("//input[@value='Upload CV']");
+		Actions builder = new Actions(driver);
+		builder.moveToElement(uploadButton).click().perform();
+		
 		Thread.sleep(10000);
+		
 		// Store the copied text in the clipboard
-		StringSelection stringSelection = new StringSelection("C:\\Users\\dell\\Desktop\\Resume.docx");
+		StringSelection stringSelection = new StringSelection("C:\\Users\\dell\\Desktop\\Jenkins_Installation.docx");
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
 
 		// Paste it using Robot class
@@ -30,31 +38,15 @@ public class UploadFileUsingRobot {
 		robot.keyPress(KeyEvent.VK_V);
 
 		robot.keyRelease(KeyEvent.VK_V);
-		robot.keyRelease(KeyEvent.VK_CONTROL);
-
+		robot.keyRelease(KeyEvent.VK_CONTROL);	
 
 		Thread.sleep(5000);
 		robot.keyPress(KeyEvent.VK_ENTER);
 		robot.keyRelease(KeyEvent.VK_ENTER);
-		
-		
-		
-		
-		
-		
-		
+			
 		Thread.sleep(3000);
-		driver.findElementByXPath("//input[@type='file']")
-		.sendKeys("C:\\Users\\dell\\Desktop\\Resume.docx");
-		//Value = "?"
+		driver.findElementByXPath("//input[@type='file']").sendKeys("C:\\Users\\dell\\Desktop\\Jenkins_Installation.docx");
+		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
